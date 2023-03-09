b683aab4a91c4eabaa06ba58ade35acb383fc5c8
Halil Betmezoglu <h.betmezoglu-1@student.tudelft.nl>
2023-03-09 18:20:19 +0100

    Fixed checkstyle

--- client/src/main/java/client/scenes/CardController.java
+++ client/src/main/java/client/scenes/CardController.java
@@ -1,21 +1,5 @@
 package client.scenes;
 
-import client.utils.ServerUtils;
-import com.google.inject.Inject;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import org.kordamp.ikonli.feather.Feather;
-import org.kordamp.ikonli.javafx.FontIcon;
-
-import javafx.event.ActionEvent;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class CardController {
 
