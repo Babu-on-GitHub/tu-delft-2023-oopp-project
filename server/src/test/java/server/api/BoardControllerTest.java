5f068e88f461e86a12f171b4f7f2dd7eba54017a
Roman Knyazhitskiy <r.knyazhitskiy@student.tudelft.nl>
2023-03-09 15:00:42 +0100

    Add more tests

--- server/src/test/java/server/api/BoardControllerTest.java
+++ server/src/test/java/server/api/BoardControllerTest.java
@@ -1,24 +1,23 @@
 package server.api;
 
+import commons.Board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import server.database.BoardRepository;
+import org.springframework.http.HttpStatus;
 
-import java.util.Random;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class BoardControllerTest {
 
-    private Random random;
     private TestBoardRepository repository;
 
     private BoardController controller;
 
     @BeforeEach
     public void setup() {
-        random = new Random();
         repository = new TestBoardRepository();
         controller = new BoardController(repository);
     }
@@ -27,5 +26,58 @@ public class BoardControllerTest {
     public void cannotAddNull(){
         var test = controller.add(null);
         assertEquals(BAD_REQUEST, test.getStatusCode());
+    }
+
+    @Test
+    public void addBoardTest1(){
+        var board = new Board();
+        var test = controller.add(board);
+        assertEquals(HttpStatus.OK, test.getStatusCode());
+
+        var all = controller.getAll();
+        assertEquals(1, all.size());
+        assertEquals(board, all.get(0));
+    }
+
+    @Test
+    public void addBoardTest2(){
+        var first = new Board();
+        first.setTitle("test");
+        first.setLists(new ArrayList<>());
+        var test = controller.add(first);
+        assertEquals(HttpStatus.OK, test.getStatusCode());
+
+        var second = new Board();
+        second.setTitle("test2");
+        second.setLists(new ArrayList<>());
+        var test2 = controller.add(second);
+        assertEquals(HttpStatus.OK, test2.getStatusCode());
+
+        var all = controller.getAll();
+        assertEquals(2, all.size());
+        assertEquals(first, all.get(0));
+        assertEquals(second, all.get(1));
+
+        assertEquals("test", all.get(0).getTitle());
+        assertEquals("test2", all.get(1).getTitle());
+    }
+
+    @Test
+    public void getByIdBoardTest() {
+        var first = new Board();
+        first.setTitle("test");
+        controller.add(first);
+
+        var second = new Board();
+        second.setTitle("test2");
+        controller.add(second);
+
+        var firstGot = controller.getById(first.getId());
+        assertNotSame(firstGot.getStatusCode(), BAD_REQUEST);
+        var secondGot = controller.getById(second.getId());
+        assertNotSame(secondGot.getStatusCode(), BAD_REQUEST);
+
+        assertEquals(first, firstGot.getBody());
+        assertEquals(second, secondGot.getBody());
     }
 }
--- server/src/test/java/server/api/CardListControllerTest.java
+++ server/src/test/java/server/api/CardListControllerTest.java
@@ -1,23 +1,23 @@
 package server.api;
 
+import commons.CardList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class CardListControllerTest {
 
-    private Random random;
     private TestCardListRepository repository;
 
     private CardListController controller;
 
     @BeforeEach
     public void setup() {
-        random = new Random();
         repository = new TestCardListRepository();
         controller = new CardListController(repository);
     }
@@ -26,5 +26,56 @@ public class CardListControllerTest {
     public void cannotAddNull(){
         var test = controller.add(null);
         assertEquals(BAD_REQUEST, test.getStatusCode());
+    }
+
+    @Test
+    public void addCardListTest1(){
+        var cardList = new CardList();
+        var test = controller.add(cardList);
+        assertEquals(HttpStatus.OK, test.getStatusCode());
+
+        var all = controller.getAll();
+        assertEquals(1, all.size());
+        assertEquals(cardList, all.get(0));
+    }
+
+    @Test
+    public void addCardListTest2(){
+        var first = new CardList();
+        first.setTitle("test");
+        first.setCards(new ArrayList<>());
+        var test = controller.add(first);
+        assertEquals(HttpStatus.OK, test.getStatusCode());
+
+        var second = new CardList();
+        second.setTitle("test2");
+        second.setCards(new ArrayList<>());
+        var test2 = controller.add(second);
+        assertEquals(HttpStatus.OK, test2.getStatusCode());
+
+        var all = controller.getAll();
+        assertEquals(2, all.size());
+        assertEquals(first, all.get(0));
+        assertEquals(second, all.get(1));
+    }
+
+    @Test
+    public void getByIdTest(){
+        var first = new CardList();
+        first.setTitle("test");
+        first.setCards(new ArrayList<>());
+        var test = controller.add(first);
+        assertEquals(HttpStatus.OK, test.getStatusCode());
+
+        var second = new CardList();
+        second.setTitle("test2");
+        second.setCards(new ArrayList<>());
+        var test2 = controller.add(second);
+        assertEquals(HttpStatus.OK, test2.getStatusCode());
+
+        var all = controller.getAll();
+        assertEquals(2, all.size());
+        assertEquals(first, all.get(0));
+        assertEquals(second, all.get(1));
     }
 }
