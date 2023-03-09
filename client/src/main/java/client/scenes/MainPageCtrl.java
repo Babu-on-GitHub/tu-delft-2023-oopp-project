45a0c8736079ac2f6a0d00fd051efb5f990a70b3
Halil Betmezoglu <h.betmezoglu-1@student.tudelft.nl>
2023-03-09 16:18:16 +0100

    Added card adding functionality

--- client/src/main/java/client/scenes/MainPageCtrl.java
+++ client/src/main/java/client/scenes/MainPageCtrl.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -41,7 +42,7 @@ public class MainPageCtrl implements Initializable {
     private StackPane card;
 
     @FXML
-    private VBox list;
+    private VBox listStartsHere;
 
     @FXML
     private HBox board;
@@ -59,11 +60,11 @@ public class MainPageCtrl implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         boardName.setText("Default board");
 
-        deleteListButton.setGraphic(new FontIcon(Feather.TRASH));
-        deleteCardButton.setGraphic(new FontIcon(Feather.TRASH));
+        //deleteListButton.setGraphic(new FontIcon(Feather.TRASH));
+        //deleteCardButton.setGraphic(new FontIcon(Feather.TRASH));
 
-        addCardButton.setGraphic(new FontIcon(Feather.PLUS));
-        addListButton.setGraphic(new FontIcon(Feather.PLUS));
+        //addCardButton.setGraphic(new FontIcon(Feather.PLUS));
+        //addListButton.setGraphic(new FontIcon(Feather.PLUS));
     }
 
     public void refresh() {
@@ -74,30 +75,29 @@ public class MainPageCtrl implements Initializable {
     public void addCardButtonPress(ActionEvent event) throws IOException {
         System.out.println("test button click");
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("client/src/main/resources/client/scenes/Card.fxml"));
-        StackPane newCard = loader.load();
 
-        //StackPane originalCard = (StackPane) loader.getNamespace().get("#card");
-
-        list.getChildren().add(newCard);
-    }
-
-    public StackPane duplicateStackPaneCard(StackPane original) {
-        StackPane card = new StackPane();
-
-        card.setAlignment(original.getAlignment());
-        card.setBlendMode(original.getBlendMode());
-        card.setMinWidth(original.getMinWidth());
-        card.setPrefWidth(original.getPrefWidth());
-        card.setStyle(original.getStyle());
-
-        return card;
+//        FXMLLoader loader = new FXMLLoader(new URL("client/src/main/resources/client/scenes/Card.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass()
+                .getResource(("Card.fxml")));
+        listStartsHere.getChildren().add(loader.load());
 
     }
+
+    @FXML
+    public void addListButtonPress(ActionEvent event) throws IOException {
+        System.out.println("test button click add card");
+
+//        FXMLLoader loader = new FXMLLoader(getClass()
+//                .getResource("client/src/main/resources/client/scenes/Card.fxml"));
+//        StackPane newCard = loader.load();
+//
+//        //StackPane originalCard = (StackPane) loader.getNamespace().get("#card");
+//
+//        listStartsHere.getChildren().add(newCard);
+    }
+
     // TODO: 08/03/2023  this is for the purpose of duplicating cards
     // a new class that will take care of duplicating cards is needed
-    // each component of the stack pane has to be separately duplicated, in the way that is done above
-    // maybe duplicators for buttons in the card would be useful
 
 
 }
