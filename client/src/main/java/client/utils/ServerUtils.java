ce8095f644b64f74c328e303c80e90df25752fbe
Roman Knyazhitskiy <r.knyazhitskiy@student.tudelft.nl>
2023-03-09 18:04:55 +0100

    Remove unused imports

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -17,10 +17,6 @@ package client.utils;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
 
 import commons.Board;
@@ -28,7 +24,6 @@ import commons.Card;
 import commons.CardList;
 import org.glassfish.jersey.client.ClientConfig;
 
-import commons.Quote;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
