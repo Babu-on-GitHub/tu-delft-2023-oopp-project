11d226bc0a74d376e320b0e3fdadb4332e74ff26
Mate Rodic <m.rodic@student.tudelft.nl>
2023-03-22 15:32:14 +0100

    Created scene for detailed card view and its prototype controller.

---
+++ client/src/main/java/client/scenes/DetailedCardController.java
@@ -0,0 +1,42 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class DetailedCardController {
+
+    @FXML
+    private TextArea cardDescription;
+
+    @FXML
+    private TextField cardTitle;
+
+    @FXML
+    private VBox detailedCardBox;
+
+    @FXML
+    private VBox subtaskArea;
+
+    @FXML
+    private Button subtaskButton;
+
+    @FXML
+    private HBox tagArea;
+
+    @FXML
+    private Button tagButton;
+
+    @FXML
+    void addSubtask(ActionEvent event) {
+        // TODO creates new subtask, displays it
+    }
+
+    @FXML
+    void addTag(ActionEvent event) {
+        // TODO creates new tag, displays it OR adds existing tag
+    }
+
+}
