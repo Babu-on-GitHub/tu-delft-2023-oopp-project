c3d4c9e4a5deb262c84b03ff097b5bf6cc64b0fb
Mate Rodic <m.rodic@student.tudelft.nl>
2023-03-28 15:42:14 +0200

    Fixed checkstyle

--- client/src/main/java/client/scenes/DetailedCardController.java
+++ client/src/main/java/client/scenes/DetailedCardController.java
@@ -1,11 +1,8 @@
-import client.scenes.CardController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
