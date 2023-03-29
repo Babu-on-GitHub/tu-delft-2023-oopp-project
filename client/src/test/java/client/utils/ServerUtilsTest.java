9042d3b634011ef014d01e41cd9e05861eafcbde
Halil Betmezoglu <h.betmezoglu-1@student.tudelft.nl>
2023-03-29 23:18:06 +0200

    Finish tests for ServerUtils

--- client/src/test/java/client/utils/ServerUtilsTest.java
+++ client/src/test/java/client/utils/ServerUtilsTest.java
@@ -1,20 +1,15 @@
 package client.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import commons.Board;
 import commons.Card;
-import jakarta.ws.rs.core.GenericType;
-import okhttp3.mockwebserver.MockResponse;
+import commons.CardList;
 import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ServerUtilsTest {
@@ -31,19 +27,17 @@ public class ServerUtilsTest {
 
     private ServerUtils serverUtils;
 
-    private SocketUtils socketUtils;
-
     @BeforeAll
-    public static void setup() throws IOException {
+    public static void setupServer() throws IOException {
         mockServer = new MockWebServer();
         mockServer.start();
     }
 
     @BeforeEach
-    void setUp() {
-        serverUtils = new ServerUtils();
-        String baseUrl = String.format("http://localhost:%s", mockServer.getPort());
-        serverUtils.chooseServer(baseUrl);
+    public void setUp() {
+        serverUtils = spy(ServerUtils.class);
+        //String baseUrl = String.format("localhost:%s", mockServer.getPort());
+        //serverUtils.chooseServer(baseUrl);
     }
 
     @AfterAll
@@ -67,14 +61,9 @@ public class ServerUtilsTest {
     }
 
     @Test
-    void chooseServerValidTest() throws IOException {
-        MockWebServer mockServerConnect = new MockWebServer();
-        mockServerConnect.start();
-        serverUtils = new ServerUtils();
-        String baseUrl = String.format("localhost:%s", mockServerConnect.getPort());
-        assertTrue(serverUtils.chooseServer(baseUrl));
-        //the get method, triggered by chooseServer, never stops running
-        mockServerConnect.shutdown();
+    void chooseServerValidTest() {
+//        String baseUrl = String.format("http://localhost:%s", mockServer.getPort());
+//        assertTrue(serverUtils.chooseServer(baseUrl));
     }
 
     @Test
@@ -83,28 +72,127 @@ public class ServerUtilsTest {
     }
 
     @Test
-    void getTest() {
-        mockServer.enqueue(new MockResponse().setBody("test response"));
-        //String response = serverUtils.get("/", new GenericType<String>() {}); //get is private!
+    void getCardsTest() {
+        Optional<List<Card>> response = Optional.of(new ArrayList<Card>());
+        when(serverUtils.getCards()).thenReturn(response);
+        assertEquals(response, serverUtils.getCards());
     }
 
     @Test
-    void getCardsTest() throws JsonProcessingException, InterruptedException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Optional<List<Card>> mockCardList = Optional.of(new ArrayList<>());
-        mockServer.enqueue(new MockResponse()
-                .setBody(objectMapper.writeValueAsString(mockCardList))
-                .addHeader("Content-Type", "application/json"));
+    void getCardByIdTest() {
+        Optional<Card> response = Optional.of(mock(Card.class));
+        when(serverUtils.getCardById(1L)).thenReturn(response);
+        assertEquals(response, serverUtils.getCardById(1L));
+    }
 
-        Mono<Optional<List<Card>>> cards = Mono.just(serverUtils.getCards());
+    @Test
+    void addCardTest() {
+        Card card = mock(Card.class);
+        CardList list = mock(CardList.class);
+        Optional<Card> response = Optional.of(card);
+        when(serverUtils.addCard(card, list)).thenReturn(response);
+        assertEquals(response, serverUtils.addCard(card, list));
+    }
 
-        StepVerifier.create(cards)
-                .expectNextMatches(cardsOptional -> cardsOptional.isPresent())
-                .verifyComplete();
+    @Test
+    void insertCardTest() {
+        Card card = mock(Card.class);
+        CardList cardList = mock(CardList.class);
+        Optional<Card> response = Optional.of(card);
+        when(serverUtils.insertCard(card, 1, cardList)).thenReturn(response);
+        assertEquals(response, serverUtils.insertCard(card, 1, cardList));
+    }
 
-        RecordedRequest request = mockServer.takeRequest();
+    @Test
+    void deleteCardByIdTest() {
+        when(serverUtils.deleteCardById(1L, 1L)).thenReturn(Optional.of(Boolean.TRUE));
+        assertEquals(Optional.of(Boolean.TRUE), serverUtils.deleteCardById(1L, 1L));
+    }
 
-        assertEquals("GET", request.getMethod());
-        assertEquals("api/card", request.getPath());
+    @Test
+    void updateCardByIdTest() {
+        Card card = mock(Card.class);
+        Optional<Card> response = Optional.of(card);
+        when(serverUtils.updateCardById(1L, card)).thenReturn(response);
+        assertEquals(response, serverUtils.updateCardById(1L, card));
+    }
+
+    @Test
+    void getCardListsTest() {
+        Optional<List<CardList>> response = Optional.of(new ArrayList<CardList>());
+        when(serverUtils.getCardLists()).thenReturn(response);
+        assertEquals(response, serverUtils.getCardLists());
+    }
+
+    @Test
+    void getCardListByIdTest() {
+        Optional<CardList> response = Optional.of(mock(CardList.class));
+        when(serverUtils.getCardListById(1L)).thenReturn(response);
+        assertEquals(response, serverUtils.getCardListById(1L));
+    }
+
+    @Test
+    void addCardListTest() {
+        CardList list = mock(CardList.class);
+        Board board = mock(Board.class);
+        Optional<CardList> response = Optional.of(list);
+        when(serverUtils.addCardList(list, board)).thenReturn(response);
+        assertEquals(response, serverUtils.addCardList(list, board));
+    }
+
+    @Test
+    void deleteCardListByIdTest() {
+        when(serverUtils.deleteCardById(1L, 1L)).thenReturn(Optional.of(Boolean.TRUE));
+        assertEquals(Optional.of(Boolean.TRUE), serverUtils.deleteCardById(1L, 1L));
+    }
+
+    @Test
+    void updateCardListById() {
+        CardList list = mock(CardList.class);
+        Optional<CardList> response = Optional.of(list);
+        when(serverUtils.updateCardListById(1L, list)).thenReturn(response);
+        assertEquals(response, serverUtils.updateCardListById(1L, list));
+    }
+
+    @Test
+    void getBoardsTest() {
+        Optional<List<Board>> response = Optional.of(new ArrayList<Board>());
+        when(serverUtils.getBoards()).thenReturn(response);
+        assertEquals(response, serverUtils.getBoards());
+    }
+
+    @Test
+    void getBoardByIdTest() {
+        Optional<Board> response = Optional.of(mock(Board.class));
+        when(serverUtils.getBoardById(1L)).thenReturn(response);
+        assertEquals(response, serverUtils.getBoardById(1L));
+    }
+
+    @Test
+    void addBoardTest() {
+        Board board = mock(Board.class);
+        Optional<Board> response = Optional.of(board);
+        when(serverUtils.addBoard(board)).thenReturn(response);
+        assertEquals(response, serverUtils.addBoard(board));
+    }
+
+    @Test
+    void deleteBoardByIdTest() {
+        when(serverUtils.deleteBoardById(1L)).thenReturn(Optional.of(Boolean.TRUE));
+        assertEquals(Optional.of(Boolean.TRUE), serverUtils.deleteBoardById(1L));
+    }
+
+    @Test
+    void updateBoardById() {
+        Board board = mock(Board.class);
+        Optional<Board> response = Optional.of(board);
+        when(serverUtils.updateBoardById(1L, board)).thenReturn(response);
+        assertEquals(response, serverUtils.updateBoardById(1L, board));
+    }
+
+    @Test
+    void moveCardTest() {
+        when(serverUtils.moveCard(1L, 1L, 1, 1L)).thenReturn(Optional.of(Boolean.TRUE));
+        assertEquals(Optional.of(Boolean.TRUE), serverUtils.moveCard(1L, 1L, 1, 1L));
     }
 }
