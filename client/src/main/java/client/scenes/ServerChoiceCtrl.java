5526eeb4e9e07e777fe6aad0a4d78b4b569d2106
Daniel Popovici <d.i.popovici-1@student.tudelft.nl>
2023-04-03 14:16:38 +0200

    Change server connection scene

--- client/src/main/java/client/scenes/ServerChoiceCtrl.java
+++ client/src/main/java/client/scenes/ServerChoiceCtrl.java
@@ -3,15 +3,33 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class ServerChoiceCtrl {
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServerChoiceCtrl implements Initializable {
     private ServerUtils utils;
 
     private MainCtrl ctrl;
 
     @FXML
     private TextField serverTextField;
+
+    @FXML
+    private AnchorPane adminPanel;
+
+    @FXML
+    private CheckBox adminCheckBox;
+
+    private boolean admin;
+
+    @FXML
+    private TextField adminPassword;
 
     @Inject
     public ServerChoiceCtrl(ServerUtils utils, MainCtrl ctrl) {
@@ -49,5 +67,18 @@ public class ServerChoiceCtrl {
 
     public void setServerTextField(TextField serverTextField) {
         this.serverTextField = serverTextField;
+    }
+
+
+    public void adminPanelControl(){
+        admin = adminCheckBox.isSelected();
+        adminPanel.setVisible(adminCheckBox.isSelected());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        adminCheckBox.setSelected(false);
+        admin = false;
+        adminPanel.setVisible(false);
     }
 }
