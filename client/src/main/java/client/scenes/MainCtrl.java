cc51578d7c36b980322125557b10472e791a1cf5
Daniel Popovici <d.i.popovici-1@student.tudelft.nl>
2023-04-04 22:37:50 +0200

    Fix bug when connecting to another server

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -50,6 +50,7 @@ public class MainCtrl {
         primaryStage.setTitle("Board Overview");
         primaryStage.setScene(mainPage);
         mainPageCtrl.setAdmin(false);
+        mainPageCtrl.initializeServerStuff();
         mainPageCtrl.refresh();
     }
 
